23d0ed798a241c5bac5d48dedb7e78efc8791c5a
Alexander Li <li.alex.0901@gmail.com>
2024-05-08 16:51:28 -0700

    Init Portfolio v2

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import styles from "./page.module.scss";
-import Shape from "../_components/Shape";
 import Logo from "../_components/Logo"
 export default function Home() {
   return (
