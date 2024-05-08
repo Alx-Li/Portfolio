c403b2e3ef5b2391146f349790dfe037a0c2334b
Alexander Li <li.alex.0901@gmail.com>
2024-05-08 16:55:21 -0700

    update file paths

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,5 +1,5 @@
 import styles from "./page.module.scss";
-import Logo from "../_components/Logo"
+import Logo from "../components/Logo"
 export default function Home() {
   return (
     <main className={styles.main}>
